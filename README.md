96f0b397d232866a88464a4523efd036b16b0f63
Noah Mayerhofer <noahmay@users.noreply.github.com>
2022-10-14 11:27:13 +0200

    First implementation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# arkad-simulator-starter-ts
+# graphaton-starter-js
+
+This is a starter project for the arkad simulator. It walks a random path and collects merch at every booth it can.
+
+## Requirements
+
+- Node.js
+
+## Installation
+
+´´´npm install´´´
+
+## Usage
+
+Run ´´´npm solve´´´ to run the solver.
---
+++ solve.ts
@@ -0,0 +1,51 @@
+const {
+  nodes,
+  relationships,
+}: {
+  nodes: Companies;
+  relationships: Relationships;
+} = require("./data.json");
+
+type CompanyId = `company${number}`;
+type Relationships = Record<
+  CompanyId,
+  Array<{ to: CompanyId; timePrice: number }>
+>;
+
+type Company = {
+  name: string;
+  swag: number;
+  timePrice: number;
+};
+
+type Companies = Record<CompanyId, Company>;
+
+const TIME_BUDGET = 4500;
+const STARTING_COMPANY_ID: CompanyId = "company25";
+
+let currentCompany = STARTING_COMPANY_ID;
+let timeLeft = TIME_BUDGET;
+let result = "";
+const visited = new Set<string>();
+
+while (timeLeft > 0) {
+  const costToVisit = nodes[currentCompany].timePrice;
+
+  if (!visited.has(currentCompany) && timeLeft >= costToVisit) {
+    visited.add(currentCompany);
+    timeLeft -= costToVisit;
+    result += `(${nodes[currentCompany].name}:collect)-->`;
+  } else {
+    result += `(${nodes[currentCompany].name})-->`;
+  }
+
+  const possiblePaths = relationships[currentCompany];
+
+  let nextPath =
+    possiblePaths[Math.floor(Math.random() * possiblePaths.length)];
+
+  timeLeft -= nextPath.timePrice;
+  currentCompany = nextPath.to;
+}
+
+console.log(result.slice(0, -3));
